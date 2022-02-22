57add18a7538e227b26dc612cae4ead00e6937f8
Azim <figaRobot@yandex.ru>
2022-02-23 00:08:38 +0300

    proper build script

--- src/main/java/icu/azim/dashboard/Dashboard.java
+++ src/main/java/icu/azim/dashboard/Dashboard.java
@@ -1,9 +1,16 @@
 package icu.azim.dashboard;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Dashboard{
+public class Dashboard extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Dashboard.class);
+	}
+    
 	public Dashboard() {
 		System.out.println("loaded dashboard class");
 	}
